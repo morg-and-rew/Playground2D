aadcb0699cb5fadcc23e30eff055f6617a2dd3a4
morg-and-rew <andreym67764@gmail.com>
2025-02-04 12:04:59 +0700

    Init 3

---
+++ Assets/Casual SCI-FI GUI Pack-Vertical Mobile/Casual SCI-FI GUI Blue/Casual SCI-FI GUI Blue Resources/Scripts/UIShowHidePanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class UIShowHidePanel : MonoBehaviour
+{
+
+    Button b;
+    
+	public List<GameObject> panels;
+	
+	public int showIndex;
+
+    public void Start()
+    {
+        b = GetComponent<Button>();
+
+        b.onClick.AddListener(Show);
+    }
+	
+	public void Show()
+	{
+		foreach(GameObject item in panels)
+		{		
+			item.SetActive(false);
+		}
+		panels[showIndex].SetActive(true);
+	}
+}
